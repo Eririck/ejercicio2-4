549d1d2cb24670ca47574e94e4d2e4d003709166
Eririck <68034950+Eririck@users.noreply.github.com>
2022-07-15 15:14:35 -0600

    tarea2-4
    erick colindres

---
+++ Ejercicio2-4/app/src/main/java/com/example/ejercicio2_4/Models/Signaturess.java
@@ -0,0 +1,21 @@
+package com.example.ejercicio2_4.Models;
+public class Signaturess {
+    private String descripcion;
+    private byte[]  image;
+    public Signaturess() {
+        this.descripcion = descripcion;
+        this.image = image;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public byte[] getImage() {
+        return image;
+    }
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
